875d276343399665cad6d08d08fab09e13a69fe1
magynhard <github@mail.magynhard.de>
2023-10-19 02:33:23 +0200

    add map infos

--- wc3_broadcast_client.rb
+++ wc3_broadcast_client.rb
@@ -1,4 +1,5 @@
 require 'socket'
+require 'stringio'
 
 # Open socket
 udp_socket = UDPSocket.new
@@ -48,6 +49,46 @@ def game_parser(message)
 
   }
 end
+
+
+
+def decode_string_part(data)
+  mask = 0
+  memory_stream = StringIO.new
+
+  first_zero_index = data.index(0)
+  data_cut = first_zero_index.nil? ? data : data[0...first_zero_index]
+
+  data_cut.each_with_index do |b, i|
+    if i % 8 != 0
+      if (mask & (1 << (i % 8))) == 0
+        memory_stream.putc(b - 1)
+      else
+        memory_stream.putc(b)
+      end
+    else
+      mask = b
+    end
+  end
+
+  memory_stream.string
+end
+
+
+def get_string_segment(data)
+  first_zero_index = data.index(0)
+  return '' if first_zero_index.nil?
+
+  string_segment = data[0...first_zero_index]
+  string_segment.pack('C*').force_encoding('UTF-8')
+end
+
+
+def utf8_byte_count(string)
+  utf8_string = string.encoding.name == 'UTF-8' ? string : string.encode('UTF-8')
+  utf8_string.bytesize
+end
+
 
 
 def request_for_lan_games(udp_socket)
@@ -105,6 +146,9 @@ loop do
   game_id = bytes[12..15].pack('C*').unpack('L<')[0]
   system_tick_counts = nil
   game_info_string = ""
+  game_settings = ""
+  zero_flag = ""
+  map_name = ""
   number_of_slots = nil
   game_flags = nil
   player_slots = nil
@@ -117,12 +161,31 @@ loop do
   game_flag = nil
   if op_code == '30'
     system_tick_counts = bytes[16..19].pack('C*').unpack('L<')[0]
-    null_termination_index = bytes[20..-1].index(0)
-    if null_termination_index
-      null_termination_index += 20
-      game_info_string = bytes[20..null_termination_index].pack('C*').unpack('Z*')[0]
-    end
-    number_of_slots = bytes[null_termination_index+1..null_termination_index+4]#.pack('C*').unpack('L<')[0]
+    game_info_bytes = bytes[20..-23]
+    game_settings = game_info_bytes[0..3].pack('C*').unpack('L<')[0]
+    zero_flag = game_info_bytes[31]
+    #game_info_string = bytes[20..-23].pack('C*').unpack('Z*')[0]
+    name = get_string_segment(bytes[20..-1])
+    encoded_segment_start_index = 22 + utf8_byte_count(name)
+    decrypted = decode_string_part(bytes[encoded_segment_start_index..-1]).bytes
+    game_settings = decrypted[0..3].pack('C*').unpack('L<')[0]
+    #map_name = game_info_bytes[39..-1].pack('C*').unpack('Z*')[0]
+    decrypted_string = decrypted.pack('C*')
+
+    # Extrahiere den Karten-Namen ab dem 14. Byte
+    map_name = decrypted_string[13..-1]
+
+    # Finde das letzte Segment des Karten-Namens, das durch '\\' getrennt ist
+    last_map_name_segment = map_name.split('\\').last
+    last_map_name_segment = last_map_name_segment[0..(last_map_name_segment.index('.w3')+4)]
+
+    # Wenn kein Segment gefunden wurde, bleibt der ursprüngliche Kartenname erhalten
+    map_name = last_map_name_segment || map_name
+
+    map_width = decrypted[5..-1].pack('C*').unpack('S<')[0]
+    map_height = decrypted[7..-1].pack('C*').unpack('S<')[0]
+
+    game_info_string = decrypted
     number_of_slots = bytes[-22..-19].pack('C*').unpack('L<')[0]
     game_flags = bytes[-18..-15].pack('C*').unpack('L<')[0]
     player_slots = bytes[-14..-11].pack('C*').unpack('L<')[0]
@@ -146,9 +209,14 @@ loop do
   puts "GAME TYPE: #{game_type}"
   puts "GAME VERSION: #{game_version}"
   puts "GAME ID: #{game_id}"
-  puts "UPTIME: #{system_tick_counts} / #{system_tick_counts.to_i/1000/60/60} hours"
+  puts "UPTIME: #{system_tick_counts}"
+  puts "GAME SETTINGS: #{game_settings}"
+  puts "NAME: #{name}"
+  puts "ZERO FLAG: #{zero_flag}"
   puts "GAME INFO: #{game_info_string}"
-  puts "null index: #{null_termination_index}"
+  puts "MAP NAME: #{map_name}"
+  puts "MAP WIDTH: #{map_width}"
+  puts "MAP HEIGHT: #{map_height}"
   puts "NUMBER OF SLOTS: #{number_of_slots}"
   puts "GAME FLAGS: #{game_flags}"
   puts "PLAYER SLOTS: #{player_slots}"
